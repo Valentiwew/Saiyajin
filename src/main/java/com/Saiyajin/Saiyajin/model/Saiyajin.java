19c94a3f4a6d0356784d4eca4775352e04e0db04
Valentiwew <bascunanbenjamin759@gmail.com>
2025-05-12 14:41:40 -0400

    Update: Class "Saiyajin"

---
+++ src/main/java/com/Saiyajin/Saiyajin/model/Saiyajin.java
@@ -0,0 +1,18 @@
+package com.Saiyajin.Saiyajin.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Saiyajin {
+
+    private int id;
+    private String nombre;
+    private String bando; /* Cambie lo de de Boolean a String para decir si es "Malo" o "Bueno" */
+    private String raza;
+    private int nivel;
+}
